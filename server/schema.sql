696ee28050d0b06137105af881ce6ffb51ea88bf
Camilo <camilogomezr25@gmail.com>
2024-05-31 17:33:53 -0500

    crear la tabla compartir_tarea y hacer comentarios

--- server/schema.sql
+++ server/schema.sql
@@ -11,10 +11,21 @@ CREATE TABLE usuarios(
     password VARCHAR(100)NOT NULL
 );
 
+--Crear la tabla tareas
 CREATE TABLE tareas(
     id INT AUTO_INCREMENT PRIMARY KEY,
     titulo VARCHAR(100) NOT NULL,
     completado BOOLEAN DEFAULT false,
     usuario_id INT NOT NULL,
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
-)
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE 
+    --elimina todas las tareas del usuario eliminado con el ON DELETE CASCADE
+);
+
+--Crear la tabla tareas compartidas (la que va en relacion a muchos a muchos)
+CREATE TABLE tareas_compartidas(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tarea_id INT,
+    usuario_id INT,
+    FOREIGN KEY (tarea_id) REFERENCES tareas(id),
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+);
